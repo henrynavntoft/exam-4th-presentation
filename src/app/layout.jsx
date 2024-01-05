f820ce0c4178d684e0779c0b2f0096c42c92d049
Henry Navntoft <henrylnavntoft@gmail.com>
2024-01-05 10:22:26 +0100

    add microsoft clarity

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,9 +2,10 @@ import "./globals.css";
 import { Providers } from "./providers";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 export const metadata = {
-  title: "4th Semester Exam",
+  title: "4th Semester Exam Presentation Update",
   description: "",
 };
 
@@ -13,6 +14,19 @@ export default function RootLayout({ children }) {
     <html lang="en" className="text-foreground bg-background">
       <body>
         <Providers>
+          <Script
+            id="clarity"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "kh0r3cpl0n");
+              `,
+            }}
+          />
           <Header />
           {children}
           <Footer />
