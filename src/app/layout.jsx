9c8712d8a3ca442fd850b5b74987d1ae2ab45aca
Henry Navntoft <henrylnavntoft@gmail.com>
2024-01-05 15:53:54 +0100

    Microsoft Clarity

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import { Providers } from "./providers";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 export const metadata = {
   title: "4th Semester Exam Presentation Update",
@@ -14,6 +15,18 @@ export default function RootLayout({ children }) {
     <html lang="en" className="text-foreground bg-background">
       <body>
         <Providers>
+          <Script
+            id="ms_clarity"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", 'kh0r3cpl0n');`,
+            }}
+          />
           <Header />
           {children}
           <Footer />
